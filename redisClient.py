0b1602c68445d168797f124a0d3a4b74cdcd0a36
wipedlifepotato <C:\Users\conta\AppData\Roaming\The Bat!>
2024-11-28 18:16:30 +0300

    feat: Docker containers

--- redisClient.py
+++ redisClient.py
@@ -1,16 +1,22 @@
 import redis, os
+from urllib.parse import urlparse
+
 class RedisSingleton:
     _instance = None  
 
     def __new__(cls):
         if cls._instance is None:
+            redis_url = os.getenv("REDIS_URL")
+            parsed_url = urlparse(redis_url)
             cls._instance = super().__new__(cls) 
+            print(f'connect to {parsed_url.hostname}:{parsed_url.port}')
             cls._instance._client = redis.Redis(
-                host=os.getenv("REDIS_HOST", "localhost"), 
-                port=int(os.getenv("REDIS_PORT", 6379)),
-                password=os.getenv("REDIS_PASSWORD"),
+                host=parsed_url.hostname or "localhost",
+                port=parsed_url.port or 6379,
+                password=parsed_url.password,
                 decode_responses=True
             )
+
         return cls._instance
 
     @property
--- run.bat
+++
@@ -1,5 +0,0 @@
-color 0c
-call venv\Scripts\activate.bat
-set FLASK_APP=main.py
-flask run
-pause
